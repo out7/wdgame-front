c7521a586a394f5c42b90592800c8d5abaf499e6
awbait <awbait.com@gmail.com>
2025-01-19 23:17:00 +0300

    feat: Просмотр респонса от запроса авторизации.

--- src/components/Root/Root.tsx
+++ src/components/Root/Root.tsx
@@ -1,11 +1,12 @@
 'use client';
 
-import {PropsWithChildren, useEffect} from "react";
+import {PropsWithChildren, useEffect, useState } from "react";
 import { init, initData } from '@telegram-apps/sdk-react';
 import { useDidMount } from "@/hooks/useDidMount";
 import { useClientOnce } from "@/hooks/useClientOnce";
 
 function RootInner({ children }: PropsWithChildren) {
+  const [authData, setAuthData] = useState(null);
 
   useClientOnce(() => {
     init();
@@ -21,10 +22,15 @@ function RootInner({ children }: PropsWithChildren) {
         },
         body: JSON.stringify({ data: initData.raw() }),
       });
+
+      const data = await response.json();
+      setAuthData(data);
     };
 
     fetchData();
   }, []);
+
+  console.log(authData);
 
   return (
     <>
